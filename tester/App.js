89dad7b9c9efe7a608b76bef320ec2abaed12c93
Mike Hatch <mihatch@c02pv5l5g8wm.reicorpnet.com>
2018-09-17 16:01:25 -0700

    working

--- tester/App.js
+++ tester/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text>My first react native app is working!</Text>
-        <Image source={{uri: 'https://facebook.github.io/react/logo-og.png'}}
-          style={{width: 400, height: 400}} />
       </View>
     );
   }
